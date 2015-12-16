c8c4aa0b70d63c1080d0c05fd23c8d65c2a63e2a
MUSHARRAF <musharraf.altamimi@gmail.com>
2015-12-17 01:12:28 +0300

    add admin interface

--- public/admin.py
+++ public/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subscribers
+
+
+class SubscribersAdmin(admin.ModelAdmin):
+    fields = ['email']
+    list_display = ('pk', 'email', )
+
+admin.site.register(Subscribers, SubscribersAdmin)
