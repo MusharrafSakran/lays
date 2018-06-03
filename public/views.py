540c6f6e68641469230ceb7e496a5baadcd8bfc8
MUSHARRAF <musharraf.altamimi@gmail.com>
2018-06-03 23:58:05 +0300

    add comment

--- public/views.py
+++ public/views.py
@@ -18,5 +18,5 @@ def subscribe(request):
         messages.error(request, 'email is not correct !')
     else:
         new_subscriber, created = Subscribers.objects.get_or_create(email=email)
-        messages.success(request, "We've got your interest, thank you!")
+        messages.success(request, "We've got your interest, thank you so much!")
     return HttpResponseRedirect(reverse('public:index'))
